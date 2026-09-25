0cb5aaf8bacc309aaebdbd0259c3111e3d3bba31
Azrazieliz <Darknessgodazrael@gmail.com>
2026-09-25 22:49:46 +0200

    Bump app to v0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.azrael.pixivdumpsync"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "android.app.Instrumentation"
     }
